66f2fb2953d3c14460001850d1d9168bc292987a
Shaban Hussein <Shabanindekupe@gmail.com>
2025-04-18 19:22:28 +0300

    Update and rename home.html to index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
         <div class="container">
-            <a class="navbar-brand" href="home.html">Home</a>
+            <a class="navbar-brand" href="index.php">Home</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" 
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -25,10 +25,10 @@
             <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link" href="login.html">Login</a>
+                        <a class="nav-link" href="login.php">Login</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="signup.html">Register</a>
+                        <a class="nav-link" href="signup.php">Register</a>
                     </li>
                 </ul>
             </div>
